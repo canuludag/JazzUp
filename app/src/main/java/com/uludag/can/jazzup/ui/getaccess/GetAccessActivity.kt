8ffbf93cb803197b55a269157d7676c3aba6fddc
Can Uludag <cucanuludag@gmail.com>
2018-10-06 21:16:33 +0300

    feat: Implement GetAccessActivity screen design

--- app/src/main/java/com/uludag/can/jazzup/ui/getaccess/GetAccessActivity.kt
+++ app/src/main/java/com/uludag/can/jazzup/ui/getaccess/GetAccessActivity.kt
@@ -1,13 +1,27 @@
 package com.uludag.can.jazzup.ui.getaccess
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.uludag.can.jazzup.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class GetAccessActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setWhiteStatusBar(mainBackground)
+    }
+
+    private fun setWhiteStatusBar(view: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            var flags = view.systemUiVisibility
+            flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            view.systemUiVisibility = flags
+            window.statusBarColor = Color.WHITE
+        }
     }
 }
