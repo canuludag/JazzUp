6e1010849415fbb881ff3e4956e037817a911204
Can Uludag <cucanuludag@gmail.com>
2018-10-06 23:25:54 +0300

    feat: Create Get Access Screen MVP contract

---
+++ app/src/main/java/com/uludag/can/jazzup/ui/getaccess/GetAccessContract.kt
@@ -0,0 +1,15 @@
+package com.uludag.can.jazzup.ui.getaccess
+
+interface GetAccessContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Model {
+
+    }
+}
